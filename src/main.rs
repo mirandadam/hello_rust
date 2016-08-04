33d44c95a2cae8b618f1b8ee26446f3007dfb775
mirandadam <mirandadam@users.noreply.github.com>
2016-08-04 18:11:50 -0300

    Implemented the guessing game tutorial.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-  println!("Hello, rust!");
+    println!("Guess a number between 1 and 100!");
+
+    let mut guess = String::new();
+    // get a random number from 1 to 100. The upper bound is not inclusive.
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("The secret number is: {}", secret_number); //for debug purposes only
+
+    loop {
+        println!("Please input your guess.");
+
+        guess.clear(); //clear previous result. read_line appends the new value to the old one.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small."),
+            Ordering::Greater => println!("Too big."),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+        }
+        // end loop.
+    }
 }
